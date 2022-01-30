b0607df8ae8ad75bff4b82c1882b78379b811621
ssl3des <ssl3des@protonmail.com>
2022-01-30 17:48:26 +0100

    bau bau

--- SpecialKProject/VirtualBody.py
+++ SpecialKProject/VirtualBody.py
@@ -3,8 +3,14 @@ import sim as s
 import simConst as sc
 import utility
 import time
+from enum import Enum
 
 FREENOVE = "Freenove4wd"
+
+
+class Clockwise(Enum):
+    RIGHT = 0
+    LEFT = 1
 
 
 class VirtualBody:
@@ -19,6 +25,8 @@ class VirtualBody:
         self._rear_left_motor = api.joint.with_velocity_control("joint_rear_left_wheel")
         self._rear_right_motor = api.joint.with_velocity_control("joint_rear_right_wheel")
         self._front_proximity_sensor = api.sensor.proximity('fps')
+        self._left_proximity_sensor = api.sensor.proximity('lps')
+        self._right_proximity_sensor = api.sensor.proximity('rps')
         self.stop()
 
         self.clientID = api._id
@@ -48,6 +56,12 @@ class VirtualBody:
         self._set_right_motors(right_velocity)
         self._set_left_motors(left_velocity)
 
+    def turn_to_right(self, right_velocity, left_velocity):
+        self.turn(- abs(right_velocity), + abs(left_velocity))
+
+    def turn_to_left(self, right_velocity, left_velocity):
+        self.turn(+ abs(right_velocity), - abs(left_velocity))
+
     # ACT
     def _set_left_motors(self, velocity):
         self._front_left_motor.set_target_velocity(velocity)
@@ -71,7 +85,19 @@ class VirtualBody:
     def get_front_distance(self):
         _, vec3 = self._front_proximity_sensor.read()
         print(vec3)
-        return vec3.get_y()
+        return vec3.distance()
+
+    # SENSE
+    def get_left_distance(self):
+        _, vec3 = self._left_proximity_sensor.read()
+        print(vec3)
+        return vec3.distance()
+
+    # SENSE
+    def get_right_distance(self):
+        _, vec3 = self._right_proximity_sensor.read()
+        print(vec3)
+        return vec3.distance()
 
     def get_orientation(self):
         self.c, self.orientation = \
@@ -88,3 +114,27 @@ class VirtualBody:
         b = utility.radians_to_degree(arr[1])
         g = utility.radians_to_degree(arr[2])
         return [a, b, g]
+
+    def rotate(self, vel, c: Clockwise):
+        arr1 = self.get_degree_orientation()
+        init_g = abs(arr1[2])
+        print(init_g)
+        stop = False
+        while not stop:
+            arr = self.get_degree_orientation()
+            print(arr)
+            curr_g = arr[2]
+            print(f"init_g: {init_g}, curr_g: {curr_g}")
+            diff = abs(abs(curr_g) - init_g)
+            print(f"Difference: {diff}")
+            if 87 < diff < 90:
+                print("90° reached")
+                stop = True
+                self.stop()
+                time.sleep(1)
+            elif c == Clockwise.RIGHT:
+                self.turn_to_right(vel, vel)
+            elif c == Clockwise.LEFT:
+                self.turn_to_left(vel, vel)
+
+
--- SpecialKProject/main.py
+++ SpecialKProject/main.py
@@ -1,7 +1,9 @@
 from pycsim import CSim, common
 
-from VirtualBody import VirtualBody
+from VirtualBody import VirtualBody, Clockwise
 import time
+import math
+
 
 if __name__ == '__main__':
     with CSim.connect("127.0.0.1", 19997) as api:
@@ -12,11 +14,24 @@ if __name__ == '__main__':
             vb.stop()
             time.sleep(2)
             print("Start")
+            vel = 45 * math.pi / 180
+            vel = 10
             while True:
-                vb.turn(-5, +5)
-                print(vb.get_degree_orientation())
+                #vb.turn(-5, +5)
+                # vb.stop()
+                # time.sleep(0.5)
+                # vb.rotate(vel, Clockwise.RIGHT)
+                # time.sleep(5)
+                # vb.stop()
+                # vb.rotate(vel, Clockwise.LEFT)
+                # time.sleep(5)
+
+                # print(vb.get_degree_orientation())
+
+                #print("distance: ", vb.get_left_distance())
+
                 # time.sleep(2)
-                # vb.move_forward(1)
+                vb.move_forward(10)
                 # time.sleep(2)
                 # vb.stop()
                 # time.sleep(1)
