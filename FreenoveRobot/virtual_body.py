0a0b6ce61929208e9b95d807b2f48e320911c434
mastrogibbs <stefano.fattore@student.univaq.it>
2022-05-10 15:03:24 +0200

    virtual body now can push messages

--- FreenoveRobot/virtual_body.py
+++ FreenoveRobot/virtual_body.py
@@ -3,12 +3,19 @@ from time import sleep
 from redis import Redis
 from physiscal_body import PhysicalBody as Body
 
+
+class TOPICS(str, Enum):
+    CONTROLLER_TOPIC = 'C_TOPIC',
+    BODY_TOPIC = 'B_TOPIC'
+
+
 class KEYS(str, Enum):
     MOTORS = 'MOTORS',
     INFRARED = 'INFRARED',
     ULTRASONIC = 'ULTRASONIC',
     BUZZER = 'BUZZER',
-    LED = 'LED'
+    LED = 'LED',
+    MPU = 'MPU'
 
 
 class VirtualBody:
@@ -20,14 +27,32 @@ class VirtualBody:
         # redis instance
         self.__redis = Redis(host='localhost', port=6379, decode_responses=True)
         self.__pubsub = self.__redis.pubsub()
-        self.__pubsub.psubscribe(**{'C_TOPIC': self.__on_message})
+        self.__pubsub.psubscribe(**{TOPICS.CONTROLLER_TOPIC.value: self.__on_message})
 
     def loop(self):
         self.__body.begin()
         self.__runner = self.__pubsub.run_in_thread(sleep_time=0.01)
 
         while True:
-            sleep(1)
+            _distances = self.__body.read_distances
+            _distances = ';'.join([str(_distances[0]), str(_distances[1]), str(_distances[2])])
+            
+            _orientation = self.__body.oritentation()
+            _yaw = _orientation[2]
+
+            _infrared = self.__body.infrared_status()
+            _infrared = ';'.join([str(_infrared[0]), str(_infrared[1]), str(_infrared[2])])
+
+            self.__redis.set(KEYS.ULTRASONIC.value, _distances)
+            self.__redis.set(KEYS.MPU.value, _yaw)
+            self.__redis.set(KEYS.INFRARED.value, _infrared)
+
+            self.__redis.publish(TOPICS.BODY_TOPIC.value, KEYS.ULTRASONIC.value)
+            self.__redis.publish(TOPICS.BODY_TOPIC.value, KEYS.MPU.value)
+            self.__redis.publish(TOPICS.BODY_TOPIC.value, KEYS.INFRARED.value)
+
+            sleep(0.01)
+
 
     def stop(self):
         self.__body.virtual_destructor()
